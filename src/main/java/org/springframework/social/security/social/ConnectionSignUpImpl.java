d98fa3bb6c1bcb3bf921d4ea7ad4b0e5548f6350
Sam Douglass <sdouglassdev@gmail.com>
2012-04-02 23:27:03 -0700

    add a big class javadoc comment, about this unused class and why it's here but unused

--- src/main/java/org/springframework/social/security/social/ConnectionSignUpImpl.java
+++ src/main/java/org/springframework/social/security/social/ConnectionSignUpImpl.java
@@ -7,6 +7,29 @@ import org.springframework.social.connect.UserProfile;
 import org.springframework.social.security.model.User;
 import org.springframework.social.security.service.UserService;
 
+/**
+ * This class can be used in two cases.
+ *
+ * 1) If you are using Spring Social's JDBC connection repository classes, you must implementing ConnectionSignUp
+ * so that JdbcUsersConnectionRepository can create a new local account for a user who signs in for the first
+ * time with a social provider. See JdbcUsersConnectionRepository.findUserIdsWithConnection(Connection<?> connection)
+ * for details.
+ *
+ * 2) If you wish to automatically create a new local account for a user signing in for the first time with a
+ * social provider, in your own implementation of UserConnectionRepository.findUserIdsWithConnection(Connection<?> connection)
+ * following the example of JdbcUsersConnectionRepository.
+ *
+ * If you have any sign up/registration requirements and validation that may not be met by the user profile data from
+ * the social provider (e.g. you require an email address, or a password, or other data) you probably do not
+ * want to make a ConnectionSignUp implementation. Rather than automatically creating an account for a user you
+ * would rely on the ProviderSignInController to send them to your sign up/registration form, where you could show
+ * them a message about what further data they must enter.
+ *
+ * In this app the sign up requirements are deliberately minimal and accounts are automatically being created. Rather
+ * than using this class though they are being created in the sign up form controller in the GET method handler.
+ *
+ * @see org.springframework.social.security.controllers.SignUpController#getForm(org.springframework.web.context.request.NativeWebRequest, org.springframework.social.security.model.User)
+ */
 public class ConnectionSignUpImpl implements ConnectionSignUp {
     @Autowired
     private UserService userService;
